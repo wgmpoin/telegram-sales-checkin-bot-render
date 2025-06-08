8e1e59a1b0ae891ef4b2d78f05d5938e8480ee52
wgmpoin <ptbis.pm1@gmail.com>
2025-06-08 14:16:24 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-echo "--- Starting custom build process ---"
-
-# Install dependencies from requirements.txt
 python3 -m pip install -r requirements.txt
-
-# Install Flask with async extra
 python3 -m pip install "Flask[async]"
-
-echo "--- Custom build process finished ---"
