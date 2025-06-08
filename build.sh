970269e8beb625f0068be4bf4dcb84f3b083e6d9
wgmpoin <ptbis.pm1@gmail.com>
2025-06-08 14:36:07 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# Update pip to the latest version first
+python3 -m pip install --upgrade pip
+
+# Install dependencies from requirements.txt
 python3 -m pip install -r requirements.txt
+
+# Install Flask with async extra
 python3 -m pip install "Flask[async]"
